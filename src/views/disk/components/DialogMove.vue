563094a326e45da7a106a95a3087db65c8cc3e56
saltbo <saltbo@foxmail.com>
2020-09-12 16:54:20 +0800

    fix: refresh the folder list on the modal open always

--- src/views/disk/components/DialogMove.vue
+++ src/views/disk/components/DialogMove.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-dialog title="移动到" width="30%" :visible.sync="visible">
-      <el-tree :props="props" node-key="id" :default-expanded-keys="[0]" :load="loadNode" :highlight-current="true" @current-change="onCurrentChange" lazy>
+      <el-tree :data="data" :props="props" node-key="id" :default-expanded-keys="[0]" :load="loadNode" :highlight-current="true" @current-change="onCurrentChange" lazy>
         <span class="custom-tree-node" slot-scope="{ node }">
           <span>
             <i class="el-icon-folder"></i>
@@ -25,9 +25,10 @@ export default {
   mixins: [mixinDialog],
   data() {
     return {
+      data: [],
       props: {
         label: "name",
-        children: "zones",
+        children: "folders",
         isLeaf: "leaf",
       },
       alias: "",
@@ -38,25 +39,24 @@ export default {
     open(obj) {
       this.alias = obj.alias;
 
+      this.loadRootForlders();
       this.visible = true;
     },
     onCurrentChange(item, node) {
       console.log(item);
       this.current = item;
     },
+    loadRootForlders() {
+      zfolder.list({ parent: "" }).then((objects) => {
+        this.data = [{ id: 0, name: "/", parent: "", folders: objects }];
+      });
+    },
     loadNode(node, resolve) {
-      console.log(node);
       if (node.level === 0) {
-        return resolve([{ id: 0, name: "全部文件", parent: "" }]);
+        return;
       }
 
-      let dir = "";
-      if (node.level > 1) dir = node.data.fullpath;
-      zfolder.list({ parent: dir }).then((objects) => {
-        setTimeout(() => {
-          return resolve(objects);
-        }, 100);
-      });
+      zfolder.list({ parent: node.data.fullpath }).then(resolve);
     },
     submit() {
       zfile.move(this.alias, this.current.fullpath).then((ret) => {
