391efb110b91fdc74ca59764ee5e6810aa328241
saltbo <saltbo@foxmail.com>
2020-10-10 23:50:28 +0800

    feat: add list refresh for the user list

--- src/views/admin/users/index.vue
+++ src/views/admin/users/index.vue
@@ -7,11 +7,15 @@
 
     <el-card shadow="never" style="margin-top: 10px; padding-bottom: 20px">
       <el-table :data="rows" size="medium" style="width: 100%">
-        <el-table-column prop="name" label="昵称" width="180"> </el-table-column>
+        <el-table-column prop="nickname" label="昵称" width="180"> </el-table-column>
         <el-table-column prop="email" label="Email" width="180"> </el-table-column>
         <el-table-column prop="role" label="角色"> </el-table-column>
         <el-table-column prop="status" label="状态"> </el-table-column>
-        <el-table-column prop="storage" label="存储配额"> </el-table-column>
+        <el-table-column prop="storage" label="存储配额">
+          <template slot-scope="scope">
+            <span>{{ scope.row.storage_used }}/ {{ scope.row.storage_max }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="operation" label="操作">
           <template slot-scope="scope">
             <el-button size="mini" @click="onEdit(scope.$index, scope.row)">编辑</el-button>
@@ -28,37 +32,27 @@
 </template>
 
 <script>
+import { zUser } from "@/libs/zpan";
 export default {
   data() {
     return {
       searchKw: "",
-      rows: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      rows: [],
+      total: 0,
     };
   },
   methods: {
+    listRefresh() {
+      zUser.list().then((ret) => {
+        this.rows = ret.data.list;
+        this.total = ret.data.total;
+      });
+    },
     onEdit(index, row) {},
     onDelete(index, row) {},
+  },
+  mounted() {
+    this.listRefresh();
   },
 };
 </script>
