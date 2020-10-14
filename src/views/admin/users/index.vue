fc59dca9572acb65a163b57c43811c3d0fd3eb6a
saltbo <saltbo@foxmail.com>
2020-10-14 23:29:23 +0800

    feat: support i18n for the page admin quota

--- src/views/admin/users/index.vue
+++ src/views/admin/users/index.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <div class="topbar">
-      <el-input v-model="query.email" @change="listRefresh" size="medium" placeholder="请输入要查找的用户邮箱" prefix-icon="el-icon-search" style="width: 300px"> </el-input>
-      <el-button type="primary" size="medium" style="float: right" disabled>添加新用户</el-button>
+      <el-input v-model="query.email" @change="listRefresh" size="medium" :placeholder="$t('admin.search-placeholder')" prefix-icon="el-icon-search" style="width: 300px"></el-input>
+      <el-button type="primary" size="medium" style="float: right" disabled>{{ $t("admin.btn-user-add") }}</el-button>
     </div>
 
     <el-card shadow="never" style="margin-top: 10px; padding-bottom: 20px">
       <el-table :data="rows" size="medium" style="width: 100%">
         <el-table-column prop="id" label="ID" width="80"> </el-table-column>
-        <el-table-column prop="nickname" label="昵称" width="120"> </el-table-column>
-        <el-table-column prop="email" label="Email"> </el-table-column>
-        <el-table-column prop="role" label="角色" width="120"> </el-table-column>
-        <el-table-column prop="status" label="状态" width="120"> </el-table-column>
-        <el-table-column prop="storage" label="存储配额" width="220">
+        <el-table-column prop="nickname" :label="$t('admin.label-nickname')" width="120"> </el-table-column>
+        <el-table-column prop="email" :label="$t('admin.label-email')"> </el-table-column>
+        <el-table-column prop="role" :label="$t('admin.label-role')" width="120"> </el-table-column>
+        <el-table-column prop="status" :label="$t('admin.label-status')" width="120"> </el-table-column>
+        <el-table-column prop="storage" :label="$t('admin.label-storage')" width="220">
           <template slot-scope="scope" width="120">
             <span>{{ scope.row.storage_used }}/{{ scope.row.storage_max }}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="operation" label="操作" width="280">
+        <el-table-column prop="operation" :label="$t('admin.label-operation')" width="280">
           <template slot-scope="scope">
-            <el-button size="mini" @click="onEdit(scope.$index, scope.row)">调整配额</el-button>
+            <el-button size="mini" @click="onEdit(scope.$index, scope.row)">{{ $t("admin.btn-quota-update") }}</el-button>
             <!-- <el-button size="mini" type="danger" @click="onDisable(scope.$index, scope.row)">禁用</el-button> -->
           </template>
         </el-table-column>
@@ -31,15 +31,15 @@
     </el-card>
 
     <!-- dialog -->
-    <el-dialog title="配额调整" :visible.sync="dialogFormVisible" width="25%">
+    <el-dialog :title="$t('admin.label-quota-change')" :visible.sync="dialogFormVisible" width="25%">
       <el-form :model="form">
-        <el-form-item label="配额大小" label-width="120px">
+        <el-form-item :label="$t('admin.label-quota')" label-width="120px">
           <el-input-number v-model="form.storage_max" :min="100" size="medium"></el-input-number>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="onStorageUpdate">确 定</el-button>
+        <el-button @click="dialogFormVisible = false">{{ $t("cancel") }}</el-button>
+        <el-button type="primary" @click="onStorageUpdate">{{ $t("confirm") }}</el-button>
       </div>
     </el-dialog>
   </div>
